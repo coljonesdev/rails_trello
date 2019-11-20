87e6478dc5cbcce6bacda9694c867255f552fadb
Colin Jones <colintjones@gmail.com>
2019-11-19 21:29:24 -0500

    Reorder action does swap order of start and end indices, but that's not quite right

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,12 +1,15 @@
 class ListsController < ApplicationController
     def reorder
         list = List.find(params[:id])
-        card1 = list.cards.find_by(order: params[:start])
-        card2 = list.cards.find_by(order: params[:end])
+        # card1 = list.cards.find_by(order: params[:start])
+        # card2 = list.cards.find_by(order: params[:end])
+        ordered_cards = (list.cards).sort_by &:order
+        card1 = ordered_cards[params[:start].to_i]
+        card2 = ordered_cards[params[:end].to_i]
 
         temp = card1.attributes['order']
-        card1.update(order: card2.attributes['order'])
-        card2.update(order: temp)
+        card1.update(order: card2.attributes['order'].to_i)
+        card2.update(order: temp.to_i)
 
         card1.save
         card2.save
