a39e9b206eef8d59b9bdcb8e43b2a2dd694dc252
Colin Jones <colintjones@gmail.com>
2019-11-19 22:06:52 -0500

    Re-ordered cards now persist on reload!

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,16 +3,28 @@ class ListsController < ApplicationController
         list = List.find(params[:id])
         # card1 = list.cards.find_by(order: params[:start])
         # card2 = list.cards.find_by(order: params[:end])
-        ordered_cards = (list.cards).sort_by &:order
-        card1 = ordered_cards[params[:start].to_i]
-        card2 = ordered_cards[params[:end].to_i]
+        # ordered_cards = (list.cards).sort_by &:order
 
-        temp = card1.attributes['order']
-        card1.update(order: card2.attributes['order'].to_i)
-        card2.update(order: temp.to_i)
+        id_array = params["cardids"].split(",")
+        id_array.each_with_index do |id, order|
+            # Grab a card with SELECT * FROM Card WHERE Id=id
+            # Update the order
+            # Save it
+            mycard = list.cards.find(id)
+            mycard.order = order
+            mycard.save
+        end
 
-        card1.save
-        card2.save
+
+        # card1 = ordered_cards[params[:start].to_i]
+        # card2 = ordered_cards[params[:end].to_i]
+
+        # temp = card1.attributes['order']
+        # card1.update(order: card2.attributes['order'].to_i)
+        # card2.update(order: temp.to_i)
+
+        # card1.save
+        # card2.save
 
         redirect_to home_index_path
     end
